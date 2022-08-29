5ec40d49e3666013aff35baa9bc8b7355d25df68
zac <zacooper5g@outlook.com>
2022-08-29 18:10:11 +1000

    updated todolist component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,42 +1,56 @@
 import { useState } from 'react';
-import { Accordion, Button, CloseButton } from 'react-bootstrap';
+import { Accordion, Button, Form } from 'react-bootstrap';
+import { TodoItems } from './TodoItems';
 
-export const TodoList = ({ todo }) => {
-  const [todoItem, setTodoItem] = useState([])
+export const TodoList = () => {
+  const [todoList, setTodoList] = useState([])
 
-  const [active, setActive] = useState(true)
+  type TodoProps = {
+    title: string;
+    notes: string;
+}
 
-  const toggleActive = () => {
-    const currentState = active;
-    setActive(!currentState)
-  }
+const todoInitialValue: TodoProps = {
+    title: '',
+    notes: '',
+}
+
+const [todo, setTodo] = useState(todoInitialValue)
+
+const onFormUpdate = (category: string, value: string) => {
+    setTodo({
+        ...todo,
+        [category]: value
+    })
+};
+
+const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const newTodoList: string[] = [todo, ...todoList]
+    setTodoList(newTodoList)
+    setTodo(todoInitialValue)
+}
 
   return (
-    <Accordion> 
-      <Accordion.Item  eventKey="0" className='d-flex justify-content-between align-items-center pe-4'>
-        <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >Accordion Item #1</Accordion.Header>{active && <CloseButton />}
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    <>
+    <Form onSubmit={handleSubmit} >
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Title" value={todo.title} onChange={(e) => onFormUpdate('title', e.target.value)}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Note</Form.Label>
+            <Form.Control as="textarea" placeholder="Add notes..." rows={3} value={todo.notes} onChange={(e) => onFormUpdate('notes', e.target.value)}/>
+        </Form.Group>
+        <Button type="submit" className="mb-3">Add Todo Item</Button>
+    </Form>
+      <Accordion> 
+      {/* {
+          todoList.map((key, title, notes) => {
+              return <TodoItems key={key} title={title} notes={notes} {...todoList} />
+          })
+      } */}
+      </Accordion>
+    </>
   );
 }
