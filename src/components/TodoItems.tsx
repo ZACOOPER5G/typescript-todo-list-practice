2bbcc6128f3289ca344ad04b9179a3e309af9c87
zac <zacooper5g@outlook.com>
2022-08-30 19:29:09 +1000

    added logic to make notes cleaner

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -11,16 +11,20 @@ type TodoItemsProps = {
 
 export const TodoItems = ({ key, title, notes, removeTodo }: TodoItemsProps) => {
 
-  return (
-      <Accordion className="gap-3 mb-4">
-        <Accordion.Item eventKey={key} >
-          <Accordion.Header >{title}
-            <span className="ps-3 pe-3" ><StatusBar key={key} /><CloseButton onClick={() => removeTodo(title)} />
-            </span></Accordion.Header>
-            <Accordion.Body>
-                {notes}
-            </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-  )
+    function capitalizeFirstLetter(string: string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+    return (
+        <Accordion className="gap-3 mb-4">
+            <Accordion.Item eventKey={key} >
+            <Accordion.Header >{capitalizeFirstLetter(title)}
+                <span className="ps-3 pe-3" ><StatusBar key={key} /><CloseButton onClick={() => removeTodo(title)} />
+                </span></Accordion.Header>
+                <Accordion.Body>
+                    {notes}
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+    )
 }
