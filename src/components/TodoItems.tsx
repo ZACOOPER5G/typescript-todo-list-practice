ccbb8e526ff46104ff1b0ed8af37fab415680f8c
zac <zacooper5g@outlook.com>
2022-08-30 17:07:20 +1000

    added status bar

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Accordion, CloseButton } from 'react-bootstrap'
+import { Accordion, CloseButton, ButtonGroup, ToggleButton } from 'react-bootstrap'
 
 type TodoItemsProps = {
     key: string;
@@ -16,11 +16,35 @@ export const TodoItems = ({ key, title, notes, removeTodo }: TodoItemsProps) => 
     const currentState = active;
     setActive(!currentState)
   }
+  const [radioValue, setRadioValue] = useState('1');
+
+  const radios = [
+    { name: 'Ongoing', value: '1' },
+    { name: 'Completed', value: '2' },
+  ];
 
   return (
       <Accordion>
         <Accordion.Item  eventKey={key} className='d-flex justify-content-between align-items-center pe-4'>
-          <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >{title}</Accordion.Header>{!active && <CloseButton onClick={() => removeTodo(title)} />}
+          <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >{title}</Accordion.Header>{
+          !active && 
+          <><ButtonGroup>
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              key={idx}
+              id={`radio-${idx}`}
+              type="radio"
+              variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {radio.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup><CloseButton onClick={() => removeTodo(title)} /></>
+          }
             <Accordion.Body>
                 {notes}
             </Accordion.Body>
