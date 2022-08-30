9639f1b97af1fbea2753c716639945346f3302c3
zac <zacooper5g@outlook.com>
2022-08-30 19:11:13 +1000

    adjusted positioning (still ugly though)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import banner from '../assets/banner.jpg'
 
 export const Header = () => {
   return (
-    <Card className="bg-light text-black d-flex justify-content-end mb-3 ">
-      <Card.Img src={banner} alt="Card image" className="h-75 w-100"/>
-      <Card.ImgOverlay className="d-flex justify-content-end align-items-center bottom-50 me-4 fs-10">
+    <Card className="bg-light text-black d-flex justify-content-end mb-3">
+      <Card.Img src={banner} alt="Card image" />
+      <Card.ImgOverlay className="d-flex justify-content-end align-items-center bottom-50 me-4 fs-10 h-75 w-100">
         <Card.Text>
           List your to do items here!
         </Card.Text>
