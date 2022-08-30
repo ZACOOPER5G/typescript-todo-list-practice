de8d10d1a63ef657c67b4e21253c6f5417014cfe
zac <zacooper5g@outlook.com>
2022-08-30 19:25:40 +1000

    removed pointless logic from component

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Accordion, CloseButton, ButtonGroup, ToggleButton } from 'react-bootstrap'
+import { StatusBar } from './StatusBar';
 
 type TodoItemsProps = {
     key: string;
@@ -10,41 +11,12 @@ type TodoItemsProps = {
 
 export const TodoItems = ({ key, title, notes, removeTodo }: TodoItemsProps) => {
 
-  const [active, setActive] = useState<Boolean>(true)
-
-  const toggleActive = () => {
-    const currentState = active;
-    setActive(!currentState)
-  }
-  const [radioValue, setRadioValue] = useState('1');
-
-  const radios = [
-    { name: 'Ongoing', value: '1' },
-    { name: 'Completed', value: '2' },
-  ];
-
   return (
-      <Accordion>
-        <Accordion.Item  eventKey={key} className='d-flex justify-content-between align-items-center pe-4'>
-          <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >{title}</Accordion.Header>{
-          !active && 
-          <><ButtonGroup>
-          {radios.map((radio, idx) => (
-            <ToggleButton
-              key={idx}
-              id={`radio-${idx}`}
-              type="radio"
-              variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-              name="radio"
-              value={radio.value}
-              checked={radioValue === radio.value}
-              onChange={(e) => setRadioValue(e.currentTarget.value)}
-            >
-              {radio.name}
-            </ToggleButton>
-          ))}
-        </ButtonGroup><CloseButton onClick={() => removeTodo(title)} /></>
-          }
+      <Accordion className="gap-3 mb-4">
+        <Accordion.Item eventKey={key} >
+          <Accordion.Header >{title}
+            <span className="ps-3 pe-3" ><StatusBar key={key} /><CloseButton onClick={() => removeTodo(title)} />
+            </span></Accordion.Header>
             <Accordion.Body>
                 {notes}
             </Accordion.Body>
