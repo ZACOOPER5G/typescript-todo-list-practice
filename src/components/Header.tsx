870f38d2408f9d28c2b05e19f8eef401eb6ae258
zac <zacooper5g@outlook.com>
2022-08-30 18:20:32 +1000

    added hideous banner for testing purposes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Card } from 'react-bootstrap'
+import banner from '../assets/banner.jpg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <Card className="bg-light text-black d-flex justify-content-end mb-3 ">
+      <Card.Img src={banner} alt="Card image" className="h-75 w-100"/>
+      <Card.ImgOverlay className="d-flex justify-content-end align-items-center bottom-50 me-4 fs-10">
+        <Card.Text>
+          List your to do items here!
+        </Card.Text>
+      </Card.ImgOverlay>
+    </Card>
   )
 }
