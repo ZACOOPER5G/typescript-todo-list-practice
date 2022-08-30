5dde5d40a82a09bb37a8ea6e3e680db922aa6143
zac <zacooper5g@outlook.com>
2022-08-30 10:44:36 +1000

    updated accordian to display correctly

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -5,9 +5,10 @@ type TodoItemsProps = {
     key: string;
     title: string;
     notes: string;
+    onClick: () => void;
 }
 
-export const TodoItems = ({ key, title, notes }: TodoItemsProps) => {
+export const TodoItems = ({ key, title, notes, onClick }: TodoItemsProps) => {
 
   const [active, setActive] = useState<Boolean>(true)
 
@@ -17,11 +18,13 @@ export const TodoItems = ({ key, title, notes }: TodoItemsProps) => {
   }
 
   return (
+      <Accordion>
         <Accordion.Item  eventKey={key} key={key} className='d-flex justify-content-between align-items-center pe-4'>
-          <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >{title}</Accordion.Header>{!active && <CloseButton />}
+          <Accordion.Header className={active ? 'active' : 'not-active'} onClick={toggleActive} >{title}</Accordion.Header>{!active && <CloseButton onClick={onClick} />}
             <Accordion.Body>
                 {notes}
             </Accordion.Body>
         </Accordion.Item>
+      </Accordion>
   )
 }
